c139022865e33d5db2fb406e2d130131434e90c7
ALEXTZSTOREBOTZ <107461989+ALEXTZSTOREBOTZ@users.noreply.github.com>
2022-06-29 16:32:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ let chalk = require('chalk')
 
 global.owner = [
   ['6285702019751'],
-  ['6285702019751', 'Owner', true]
+  ['6285702019751', 'DITZ BOT', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
